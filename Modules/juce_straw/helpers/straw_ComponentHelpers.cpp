21753f5db8e09515753f46819bcf4af75fa774bc
kunitoki <kunitoki@gmail.com>
2024-06-14 08:53:09 +0200

    Updated licensing

--- Modules/juce_straw/helpers/straw_ComponentHelpers.cpp
+++ Modules/juce_straw/helpers/straw_ComponentHelpers.cpp
@@ -1,6 +1,23 @@
-/**
- * straw 4 the juce - Copyright (c) 2024, Lucio Asnaghi. All rights reserved.
- */
+/*
+  ==============================================================================
+
+   This file is part of the straw project.
+   Copyright (c) 2024 - kunitoki@gmail.com
+
+   straw is an open source library subject to open-source licensing.
+
+   The code included in this file is provided under the terms of the ISC license
+   http://www.isc.org/downloads/software-support-policy/isc-license. Permission
+   to use, copy, modify, and/or distribute this software for any purpose with or
+   without fee is hereby granted provided that the above copyright notice and
+   this permission notice appear in all copies.
+
+   STRAW IS PROVIDED "AS IS" WITHOUT ANY WARRANTY, AND ALL WARRANTIES, WHETHER
+   EXPRESSED OR IMPLIED, INCLUDING MERCHANTABILITY AND FITNESS FOR PURPOSE, ARE
+   DISCLAIMED.
+
+  ==============================================================================
+*/
 
 #include "straw_ComponentHelpers.h"
 
